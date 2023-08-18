4d6152ecc7a1fca7f343df7845134f535f81fc38
Kanderson58 <kanderson8585@gmail.com>
2023-08-17 19:33:29 -0600

    Set up firebase BE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, getDoc, doc, setDoc, updateDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_bUvgzrtGDKakaXi0AswV1v-o3T2lnHg",
+  authDomain: "go-gamers-f5d9b.firebaseapp.com",
+  projectId: "go-gamers-f5d9b",
+  storageBucket: "go-gamers-f5d9b.appspot.com",
+  messagingSenderId: "628130654826",
+  appId: "1:628130654826:web:11a5fa3293e0d1d985f813"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 let trail = [];
 const logoMap = {
@@ -17,7 +31,7 @@ const getTrafficCones = () => {
   const coneLocations = [];
   for(var i = 0; i < (Math.ceil(Math.random() * (45 - 20) + 20)); i++) {
     const randomPos = {top: getRandomWindowPos(0, window.innerHeight), left: getRandomWindowPos(0, window.innerWidth)};
-    cones.push(<img src={require('./traffic.png')} alt="traffic cone" className='traffic' style={{top: randomPos.top, left: randomPos.left}} />)
+    cones.push(<img src={require('./traffic.png')} alt="traffic cone" className='traffic' style={{top: randomPos.top, left: randomPos.left}} key={`${randomPos.top}${randomPos.left}`} />)
     coneLocations.push(randomPos);
   }
   return {cones: cones, coneLocations: coneLocations};
@@ -25,19 +39,44 @@ const getTrafficCones = () => {
 
 let cones;
 
+const getScores = async () => {
+  const response = doc(db, 'wins', 'HYqMpv64MGHf36oWwGR1');
+  const collectionSnapshot = await getDoc(response);
+  const scores = collectionSnapshot.data();
+  return scores;
+}
+
 function App() {
   const [pos, setPos] = useState({ });
   const [logo, setLogo] = useState('golinks');
   const [hit, setHit] = useState(false);
   const [gameStart, setGameStart] = useState(false);
   const [result, setResult] = useState('');
-
+  const [scores, setScores] = useState();
+  
   useEffect(() => {
     setTimeout(() => {
       trail = (trail.length > 0) ? trail.slice(1) : trail;
     }, 500);
   }, [pos]);
 
+  useEffect(() => {
+    if(!scores) return;
+    const updateScores = async () => {
+      const scoreRef = doc(db, "wins", "HYqMpv64MGHf36oWwGR1");
+      await updateDoc(scoreRef, {
+        [logo]: scores[logo]
+      });
+    }
+
+    updateScores();
+  }, [scores])
+
+  const scoreGetter = async () => {
+    const updatedScores = await getScores();
+    return updatedScores;
+  }
+  
   const checkIfClose = (e) => {
     if((e.clientX - pos.X) > 10 || (e.clientX - pos.X) > -10) return true;
     if((e.clientY - pos.Y) > 10 || (e.clientY - pos.Y) > -10) return true;
@@ -58,8 +97,8 @@ function App() {
     if (checkIfClose(e)) return;
     setPos({ x: e.clientX - 10, y: e.clientY + 10 })
     if (!pos.x || !pos.y) return;
-    if(pos.x === e.clientX && pos.y === e.clientY) return;
-    trail.push(<img src={require(`${logoMap[logo]}`)} alt="logo" className='logo' style={{position: 'absolute', top: pos.y, left: pos.x}} />)
+    if(trail.find(img => img.key === `${pos.y}${pos.x}`)) return;
+    trail.push(<img src={require(`${logoMap[logo]}`)} alt="logo" className='logo' style={{position: 'absolute', top: pos.y, left: pos.x}} key={`${pos.y}${pos.x}`} />)
     if(!gameStart) return;
     checkForCones(e);
   };
@@ -69,30 +108,33 @@ function App() {
     trail = [];
   }
 
-  const startGame = () => {
+  const startGame = async () => {
+    const newScores = await scoreGetter();
+    setScores(newScores);
     setGameStart(true);
     cones = getTrafficCones();
     trail = [];
   }
 
-  const winGame = () => {
+  const winGame = async () => {
+    setScores({...scores, [logo]: scores[logo] + 1});
     setGameStart(false);
     setResult('You win! 🏆');
   }
-
-  const restartGame = () => {
+  
+  const restartGame = async () => {
     setHit(false);
     setGameStart(false);
     setLogo('golinks');
     setResult('');
     trail = [];
-    cones = [];
+    cones = [];    
   }
-
+  
   return (
     <div className='app-container'>
       {!result && <button style={{top: 25, left: 25}} onClick={startGame}>Start</button>}
-      <div className="app" onMouseMove={moveTrail}>
+      <div className="app" onMouseMove={moveTrail} onTouchStart={moveTrail}>
         {trail.map(img => img)}
           {!result && <>
             {gameStart && cones.cones}
@@ -104,6 +146,11 @@ function App() {
           </>}
           {result && <div className='result-container'>
             <p className='result'>{result}</p>
+            <ul>
+              <li>GoLinks: {scores.golinks}</li>
+              <li>GoSearch: {scores.gosearch}</li>
+              <li>GoProfiles: {scores.goprofiles}</li>
+            </ul>
             <button onClick={restartGame} className='reset'>Reset</button>
           </div>}
       </div>
